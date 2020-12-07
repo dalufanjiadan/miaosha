f763c4061cf1bb29578dbe34d2236e4198a5c40f
dalufanjiadan <dalufanjiadan@qq.com>
2020-12-07 14:40:14 +0800

    spring cache asyncconfig

---
+++ src/main/java/com/example/demo/config/AsyncConfig.java
@@ -0,0 +1,43 @@
+package com.example.demo.config;
+
+import java.util.concurrent.Executor;
+
+import com.example.demo.exception.GlobalAsyncExceptionHandler;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+	@Autowired
+	private GlobalAsyncExceptionHandler exceptionHandler;
+
+	@Override
+	public Executor getAsyncExecutor() {
+		// 定义线程池
+		ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+		// 设置核心线程数
+		threadPoolTaskExecutor.setCorePoolSize(10);
+		// 设置线程池最大线程数
+		threadPoolTaskExecutor.setMaxPoolSize(30);
+		// 设置线程队列最大线程数
+		threadPoolTaskExecutor.setQueueCapacity(2000);
+		// 初始化线程池
+		threadPoolTaskExecutor.initialize();
+
+		return threadPoolTaskExecutor;
+
+	}
+
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		return exceptionHandler;
+	}
+
+}
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -6,9 +6,13 @@ import com.example.demo.model.User;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 
 @Mapper
+@CacheConfig(cacheNames = "user")
 public interface UserMapper {
 
+	@Cacheable(key = "#p0")
 	public Optional<User> findById(int userId);
 }
